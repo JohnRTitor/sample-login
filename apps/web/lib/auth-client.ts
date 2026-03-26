a2d9f5e347e73ea1cc985ba9613d65cffefc68c9
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2026-03-26 09:59:52 +0530

    update devenv, prisma, next, turbo, better-auth
    update type of authClient

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL ?? "",
-});
+}) as ReturnType<typeof createAuthClient>;
 
 export const { signIn, signUp, signOut, useSession } = authClient;
